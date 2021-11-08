79a75d4e7529658ca0db66315766d3a2e1c06227
laraiche Mohamed <laraichemohamed@gmail.com>
2021-11-08 02:23:43 +0000

    THE END

--- javaScript/app.js
+++ javaScript/app.js
@@ -4,30 +4,3 @@ let list = document.querySelector(".list");
 burger.addEventListener("click", () => {
   list.classList.toggle("activeNav");
 });
-
-// Send EMAIL
-
-let sendBtn = document.querySelector(".btn-submit");
-const emailInput = document.querySelector("form");
-sendBtn.addEventListener("click", () => {
-  Email.send({
-    Host: "smtp.mailtrap.io",
-    UserName: "6baf9ae9e696f2",
-    Password: "2b6c0258468531",
-    To: "laraichemohamed@gmail.com",
-    From: emailInput.elements["email"].value,
-    Subject: "Demande Devis",
-    body:
-      emailInput.elements["msg"].value +
-      "< br>" +
-      emailInput.elements["nom"].value +
-      "<br>" +
-      emailInput.elements["prenom"].value +
-      "<br>" +
-      emailInput.elements["ste"].value +
-      "<br>" +
-      emailInput.elements["phone"].value,
-  }).then((msg) => {
-    alert("The Demande has been Send thank you we will replied you in 24h");
-  });
-});
